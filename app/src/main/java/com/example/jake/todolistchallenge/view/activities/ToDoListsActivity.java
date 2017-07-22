d21608b4d3fa0e308193361e56c1e6833136b969
Jake Rushing <jakester0812@gmail.com>
2017-07-22 01:05:54 -0700

    Added Activities, view xmls, and hierarchy arranged in MVP architecture

---
+++ app/src/main/java/com/example/jake/todolistchallenge/view/activities/ToDoListsActivity.java
@@ -0,0 +1,15 @@
+package com.example.jake.todolistchallenge.view.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jake.todolistchallenge.R;
+
+public class ToDoListsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do_lists);
+    }
+}
