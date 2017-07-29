e8c3ab5778ee8e6b4ff782fe5d05d4dc86ff2498
Jake Rushing <jakester0812@gmail.com>
2017-07-29 08:10:40 -0700

    Adding some things to the project

---
+++ app/src/main/java/com/example/jake/todolistchallenge/constants/ToDoConstants.java
@@ -0,0 +1,13 @@
+package com.example.jake.todolistchallenge.constants;
+
+/**
+ * Created by Jake on 7/23/2017.
+ */
+
+public class ToDoConstants {
+
+    /*CLASS VARIABLES ________________________________ */
+
+    //DATABASE CONSTANTS
+    public static final String DATABASE_NAME = "ToDoListDatabase";
+}
---
+++ app/src/main/java/com/example/jake/todolistchallenge/database/ToDoListsDatabaseHelper.java
@@ -0,0 +1,8 @@
+package com.example.jake.todolistchallenge.database;
+
+/**
+ * Created by Jake on 7/23/2017.
+ */
+
+public class ToDoListsDatabaseHelper extends SQLiteAssetHelper{
+}
