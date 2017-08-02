8e9e44ad5c3058d79eeb9d335a32122971ad23d9
Jake Rushing <jakester0812@gmail.com>
2017-08-02 00:54:53 -0700

    Added Database classes and Base Presenter interfaces

---
+++ app/src/main/java/com/example/jake/todolistchallenge/database/ToDoListContract.java
@@ -0,0 +1,9 @@
+package com.example.jake.todolistchallenge.database;
+
+import android.provider.BaseColumns;
+/**
+ * Created by Jake on 8/2/2017.
+ */
+
+public class ToDoListContract implements BaseColumns {
+}
---
+++ app/src/main/java/com/example/jake/todolistchallenge/presenters/BasePresenter.java
@@ -0,0 +1,11 @@
+package com.example.jake.todolistchallenge.presenters;
+
+/**
+ * Created by Jake on 8/2/2017.
+ */
+
+public interface BasePresenter {
+
+    void subscribe();
+    void unsubscribe();
+}
